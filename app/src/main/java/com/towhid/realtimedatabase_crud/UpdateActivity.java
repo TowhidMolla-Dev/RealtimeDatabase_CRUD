c35edfa088378eb5539b395d2fa9502645f71d3a
TowhidMolla-Dev <towhidm74@gmail.com>
2024-01-09 19:57:47 +0600

    Initial commit

---
+++ app/src/main/java/com/towhid/realtimedatabase_crud/UpdateActivity.java
@@ -0,0 +1,13 @@
+package com.towhid.realtimedatabase_crud;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UpdateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update);
+    }
+}
